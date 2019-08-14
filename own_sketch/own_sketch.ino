0b9544b5dab8200e9bd477fff55ab21d137e1041
earyzhe <earyzhe@gmail.com>
2019-08-14 16:45:24 +0200

    Working with logs but when the state of the pin changes the program stops working

--- own_sketch/own_sketch.ino
+++ own_sketch/own_sketch.ino
@@ -4,37 +4,49 @@
 */
 
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-#ifndef STASSID
-#define STASSID "your-ssid"
-#define STAPSK  "your-password"
-#endif
+// For Temparature
+#include "DHTesp.h"
+uint8_t DHTPin = 4;
+DHTesp dht;
+float Temperature;
+float Humidity;
 
-const char* ssid     = STASSID;
-const char* password = STAPSK;
+uint8_t LED_ONE = 2;
 
-const char* host = "djxmmx.net";
-const uint16_t port = 17;
+// WIFI
+#include <ESP8266WiFi.h>
+const char* ssid = "(Don't mention the war)";
+const char* password = "56939862460419967485";
 
+// MQTT
+const char *mqtt_server = "postman.cloudmqtt.com";
+#define mqtt_port 11968
+#define MQTT_USER "xxtdtmwf"
+#define MQTT_PASSWORD "c-0_VSx4qaOv"
+#define MQTT_SERIAL_PUBLISH_TEST "test"
+#define MQTT_SERIAL_PUBLISH_PLANTS "plants/berlin/oderstrasse/andrew"
+#define MQTT_SERIAL_PUBLISH_CPU "things/esp8266"
+#define MQTT_SERIAL_PUBLISH_PLACE "places/berlin/oderstrasse/andrew"
 
 const int BAUD = 115200;
 const int LOOP_DELAY = 2000;
 const int PUMP_PIN = 5;
 
 // Time in milliseconds
-const unsigned long  loopDelayNormalSecs = 600;
+const unsigned long loopDelayNormalSecs = 600;
 //const unsigned long  loopDelayNormalSecs = 6;
 const int loopDelayPumpmilli = 100;
 
 int initialised = 0;
 
-
 class Module
 {
-  public:
-    Module( char, char*, int, int, int, int, int, int, int, bool);
+public:
+    Module(char, char *, int, int, int, int, int, int, int, bool);
     char id;
-    char* plantType;
+    char *plantType;
     int moistureSettingLow;
     int moistureSettingHigh;
     int sensorLowerValue;
@@ -43,26 +55,24 @@ class Module
     int currentPercentage;
     int servoPin;
     bool isPumping;
-
 };
 
 /// Constructor for each module
-Module::Module(char a, char* b, int c, int d, int e, int f, int g, int h, int i, bool j )
+Module::Module(char a, char *b, int c, int d, int e, int f, int g, int h, int i, bool j)
 {
-  id = a;
-  plantType = b;
-  readPin = c;
-  servoPin = d;
-  moistureSettingLow = e;
-  moistureSettingHigh = f;
-  sensorLowerValue = g;
-  sensorUpperValue = h;
-  currentPercentage = i;
-  isPumping = j;
+    id = a;
+    plantType = b;
+    readPin = c;
+    servoPin = d;
+    moistureSettingLow = e;
+    moistureSettingHigh = f;
+    sensorLowerValue = g;
+    sensorUpperValue = h;
+    currentPercentage = i;
+    isPumping = j;
 }
 
 Module plantOne = Module('1', "Unknown", 0, 5, 40, 70, 12, 449, 0, false);
-
 
 //#define MODULE_COUNT 7
 //Module modules[MODULE_COUNT] = {
@@ -76,219 +86,198 @@ Module plantOne = Module('1', "Unknown", 0, 5, 40, 70, 12, 449, 0, false);
 ////    Module(A6, 0, '7', 70, 8, 597, 287, 40, false, "peace_lily"), // Checked sensor values 8/5/2019 Peace Lily
 //};
 
-
-void setup() {
-  setupSerial();
-  setupPins();
+void setup()
+{
+    setupSerial();
+    setupPins();
 }
 
-void loop() {
-  readSensors();
-  delay(LOOP_DELAY);
+void loop()
+{
+    readSensors();
+    delay(LOOP_DELAY);
 }
 
-void readSensors() {
+void readSensors()
+{
 
-  bool needsPump = false;
+    char finalString[400];
 
-  //    for (int i = 0; i < MODULE_COUNT; i++)
-  //    {
-  //      Module currentModule = modules[i];
-  Module currentModule = plantOne;
-  // Project /
-  Serial.print("<plant_system");
-  //Tags /
-  Serial.print(",city=Berlin");
-  Serial.print(",location=oderstrasse");
-  Serial.print(",room=andrews");
-  Serial.print(",plant_type=");
-  Serial.print(currentModule.plantType);
-  Serial.print(",plant_id=");
-  Serial.print(currentModule.id);
+    bool needsPump = false;
 
-  Serial.print(" ");
+    //    for (int i = 0; i < MODULE_COUNT; i++)
+    //    {
+    //      Module currentModule = modules[i];
+    Module currentModule = plantOne;
 
-  //Fields
-  Serial.print("servo_pin=");
-  Serial.print(currentModule.servoPin);
+    strcpy(finalString, "plant_system,city=Berlin,city=Berlin,room=andrews,thing=esp8266,plant_type=");
+  
+    strcat(finalString, currentModule.plantType);
 
-  Serial.print(",read_pin=");
-  Serial.print(currentModule.readPin);
+    strcat(finalString, ",plant_id=");
 
-  Serial.print(",sensor_reading=");
-  Serial.print(analogRead(currentModule.readPin));
+    char z[20];
+    snprintf(z, 20, "%c", currentModule.id);
+    strcat(finalString, z);
 
-  Serial.print(",moi_setting_high=");
-  Serial.print(currentModule.moistureSettingHigh);
+    strcat(finalString, " ");
 
-  Serial.print(",moi_setting_low=");
-  Serial.print(currentModule.moistureSettingLow);
+    //Fields
+    strcat(finalString, "servo_pin=");
+    char a[20];
+    snprintf(a, 20, "%ld", currentModule.servoPin);
+    strcat(finalString, a);
 
-  Serial.print(",sensor_low_value=");
-  Serial.print(currentModule.sensorLowerValue);
+    strcat(finalString, ",read_pin=");
+    char b[20];
+    snprintf(b, 20, "%ld", currentModule.readPin);
+    strcat(finalString, b);
 
-  Serial.print(",sensor_high_value=");
-  Serial.print(currentModule.sensorUpperValue);
+    strcat(finalString, ",sensor_reading=");
+    char c[20];
+    snprintf(c, 20, "%ld", analogRead(currentModule.readPin));
+    strcat(finalString, c);
 
+    strcat(finalString, ",moi_setting_high=");
+    char d[20];
+    snprintf(d, 20, "%ld", currentModule.moistureSettingHigh);
+    strcat(finalString, d);
 
-  currentModule.currentPercentage = convertToPercent(analogRead(currentModule.readPin), currentModule);
+    strcat(finalString, ",moi_setting_low=");
+    char e[20];
+    snprintf(e, 20, "%ld", currentModule.moistureSettingLow);
+    strcat(finalString, e);
 
-  Serial.print(",moisture_level=");
-  Serial.print(currentModule.currentPercentage);
+    strcat(finalString, ",sensor_low_value=");
+    char f[20];
+    snprintf(f, 20, "%ld", currentModule.sensorLowerValue);
+    strcat(finalString, f);
 
-  if (currentModule.currentPercentage < currentModule.moistureSettingLow)
-  {
-    currentModule.isPumping = true;
-    needsPump = true;
-    digitalWrite(currentModule.servoPin, LOW);
+    strcat(finalString, ",sensor_high_value=");
+    char g[20];
+    snprintf(g, 20, "%ld", currentModule.sensorUpperValue);
+    strcat(finalString, g);
 
-    //Opening servo
-    Serial.print(",dead_zone=0");
-  }
-  if (currentModule.currentPercentage >= currentModule.moistureSettingLow && currentModule.currentPercentage <= currentModule.moistureSettingHigh)
-  {
-    if (!currentModule.isPumping)
+    currentModule.currentPercentage = convertToPercent(analogRead(currentModule.readPin), currentModule);
+
+    strcat(finalString, ",moisture_level=");
+    char h[20];
+    snprintf(h, 20, "%ld", currentModule.currentPercentage);
+    strcat(finalString, h);
+
+    if (currentModule.currentPercentage < currentModule.moistureSettingLow)
     {
-      digitalWrite(currentModule.servoPin, HIGH);
+        currentModule.isPumping = true;
+        needsPump = true;
+        digitalWrite(currentModule.servoPin, LOW);
+
+        //Opening servo
+        strcat(finalString, ",dead_zone=0");
+    }
+    if (currentModule.currentPercentage >= currentModule.moistureSettingLow && currentModule.currentPercentage <= currentModule.moistureSettingHigh)
+    {
+        if (!currentModule.isPumping)
+        {
+            digitalWrite(currentModule.servoPin, HIGH);
+        }
+
+        //the deadzone
+        strcat(finalString, ",dead_zone=1");
+    }
+    if (currentModule.currentPercentage > currentModule.moistureSettingHigh)
+    {
+        currentModule.isPumping = false;
+        digitalWrite(currentModule.servoPin, HIGH);
+
+        //Opening servo
+        strcat(finalString, ",dead_zone=0");
     }
 
-    //the deadzone
-    Serial.print(",dead_zone=1");
-  }
-  if (currentModule.currentPercentage > currentModule.moistureSettingHigh)
-  {
-    currentModule.isPumping = false;
-    digitalWrite(currentModule.servoPin, HIGH);
-
-    //Opening servo
-    Serial.print(",dead_zone=0");
-  }
-
-  byte servoPinState = digitalRead(currentModule.servoPin);
-  if (servoPinState == LOW)
-  {
-    Serial.print(",servo=1");
-  }
-  else
-  {
-    Serial.print(",servo=0");
-  }
-
-  byte pumpPinState = digitalRead(PUMP_PIN);
-
-  if (pumpPinState == LOW)
-  {
-    Serial.println(",pump=0>");
-  }
-  else
-  {
-    Serial.println(",pump=1>");
-  }
-  delay(100);
-  // EnMd of loop
-  //    }
-
-  if (initialised >= 5) {
-
-    if (needsPump)
+    byte servoPinState = digitalRead(currentModule.servoPin);
+    if (servoPinState == LOW)
     {
-      digitalWrite(PUMP_PIN, HIGH);
-      delay(loopDelayPumpmilli);
+        strcat(finalString, ",servo=1");
     }
     else
     {
-      digitalWrite(PUMP_PIN, LOW);
-      int p = 0;
-      while (p < loopDelayNormalSecs) {
-        delay(1000);
-        p++;
-      }
+        strcat(finalString, ",servo=0");
     }
 
-  } else {
-    initialised ++;
-  }
+    byte pumpPinState = digitalRead(PUMP_PIN);
+
+    if (pumpPinState == LOW)
+    {
+        strcat(finalString, ",pump=0");
+    }
+    else
+    {
+        strcat(finalString, ",pump=1");
+    }
+    delay(100);
+    Serial.println(finalString);
+    // EnMd of loop
+    //    }
+
+//    if (initialised >= 5)
+//    {
+//
+//        if (needsPump)
+//        {
+//            digitalWrite(PUMP_PIN, HIGH);
+//            delay(loopDelayPumpmilli);
+//        }
+//        else
+//        {
+//            digitalWrite(PUMP_PIN, LOW);
+//            int p = 0;
+//            while (p < loopDelayNormalSecs)
+//            {
+//                delay(1000);
+//                p++;
+//            }
+//        }
+//    }
+//    else
+//    {
+//        initialised++;
+//    }
 }
 
 int convertToPercent(int sensorValue, Module module)
 {
-  int percentValue = map(sensorValue, module.sensorLowerValue, module.sensorUpperValue, 0, 100);
-  return percentValue;
+    int percentValue = map(sensorValue, module.sensorLowerValue, module.sensorUpperValue, 0, 100);
+    return percentValue;
 }
 
-void setupPins() {
-  pinMode(plantOne.readPin, INPUT);
-  pinMode(plantOne.servoPin, OUTPUT);
-  pinMode(PUMP_PIN, OUTPUT);
+void setupPins()
+{
+    pinMode(plantOne.readPin, INPUT);
+    pinMode(plantOne.servoPin, OUTPUT);
+    pinMode(PUMP_PIN, OUTPUT);
 }
 
-void setupSerial() {
-  Serial.begin(115200);
-  Serial.print("Connecting to ");
+void setupSerial()
+{
+    Serial.begin(115200);
+    Serial.print("Connecting to ");
 }
 
-void setupWifi() {
+void setupWifi()
+{
 
-  Serial.println(ssid);
-  /* Explicitly set the ESP8266 to be a WiFi-client */
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+    Serial.println(ssid);
+    /* Explicitly set the ESP8266 to be a WiFi-client */
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-
-void wifiloop() {
-  Serial.print("connecting to ");
-  Serial.print(host);
-  Serial.print(':');
-  Serial.println(port);
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  if (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    delay(5000);
-    return;
-  }
-
-  // This will send a string to the server
-  Serial.println("sending data to server");
-  if (client.connected()) {
-    client.println("hello from ESP8266");
-  }
-
-  // wait for data to be available
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      delay(60000);
-      return;
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
     }
-  }
 
-  // Read all the lines of the reply from server and print them to Serial
-  Serial.println("receiving from remote server");
-  // not testing 'client.connected()' since we do not need to send data here
-  while (client.available()) {
-    char ch = static_cast<char>(client.read());
-    Serial.print(ch);
-  }
-
-  // Close the connection
-  Serial.println();
-  Serial.println("closing connection");
-  client.stop();
-
-  delay(300000); // execute once every 5 minutes, don't flood remote service
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
